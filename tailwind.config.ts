e2a3fb2424a6467816fd24e63d1a75faa3877b37
ke9sh1n <k3nshi9@gmail.com>
2025-01-28 16:25:32 +0600

    initialized gbgaid projects folder

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,23 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				background: "var(--background)",
+				foreground: "var(--foreground)",
+			},
+			fontFamily: {
+				worksans: ["var(--font-work-sans)", "sans-serif"],
+				script: ["var(--font-sacramento)", "cursive"],
+			},
+		},
+	},
+	plugins: [],
 };
 export default config;
