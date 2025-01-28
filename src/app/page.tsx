26ae14cb4f0bf4a589c4317d9bd7a6f04edae603
ke9sh1n <k3nshi9@gmail.com>
2025-01-28 18:06:33 +0600

    setup css configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
 	return (
 		<main>
-			<h1 className=" font-worksans">Hello World!</h1>
+			<h1 className=" font-worksans responsive-text">Hello World!</h1>
 		</main>
 	);
 }
